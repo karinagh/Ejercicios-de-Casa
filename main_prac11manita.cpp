553e249025602fdcfe1758a5dc36e3ce6254eb72
karinagh <42215777+karinagh@users.noreply.github.com>
2018-10-28 20:34:22 -0600

    Manita movimientos
    La animación de los movimientos estaba rara (temblorosa) y no se almacenaban todos los movimientos guardados, por eso los dedos se ven raros, esto es lo máximo que pude lograr en posición de amor y paz y de rock.

---
+++ main_prac11manita.cpp
@@ -0,0 +1,657 @@
+//Semestre 2019- 1
+//************************************************************//
+//************************************************************//
+//************** Alumno (s): *********************************//
+//***************	GALVÁN HERNÁNDEZ KARINA ******************//
+//***************	VISUAL STUDIO 2015 ***********************//
+//************************************************************//
+//************************************************************//
+
+#include "texture.h"
+#include "figuras.h"
+#include "Camera.h"
+
+#include "cmodel/CModel.h"
+
+//NEW//////////////////NEW//////////////////NEW//////////////////NEW////////////////
+static GLuint ciudad_display_list;	//Display List for the Monito
+
+
+									//NEW// Keyframes
+									//Variables de dibujo y manipulacion
+float posX = 0, posY = 2.5, posZ = -3.5, rotRodIzq = 0;
+float giroMonito = 0;
+float rotBrIzq = 0, rotBrDer = 0, rotRodDer = 0; //variables para rotacion de brazos
+
+
+#define MAX_FRAMES 5
+int i_max_steps = 90;
+int i_curr_steps = 0;
+
+
+typedef struct _frame
+{
+	//Variables para GUARDAR Key Frames
+	float posX;		//Variable para PosicionX
+	float posY;		//Variable para PosicionY
+	float posZ;		//Variable para PosicionZ
+	float incX;		//Variable para IncrementoX
+	float incY;		//Variable para IncrementoY
+	float incZ;		//Variable para IncrementoZ
+	float rotRodIzq;
+	float rotInc;
+	float giroMonito;
+	float giroMonitoInc;
+	float rotInc2;
+	float rotBrIzq;
+	float rotBrDer;
+	float rotRodDer;
+	float rotInc3, rotInc4;
+
+
+}FRAME;
+
+FRAME KeyFrame[MAX_FRAMES];
+int FrameIndex = 0;			//introducir datos
+bool play = false;
+int playIndex = 0;
+
+
+//NEW//////////////////NEW//////////////////NEW//////////////////NEW////////////////
+
+int w = 500, h = 500;
+int frame = 0, time, timebase = 0;
+char s[30];
+
+CCamera objCamera;	//Create objet Camera
+
+GLfloat g_lookupdown = 0.0f;    // Look Position In The Z-Axis (NEW) 
+
+int font = (int)GLUT_BITMAP_HELVETICA_18;
+
+
+//GLfloat Diffuse[]= { 1.0f, 1.0f, 1.0f, 1.0f };				// Diffuse Light Values
+GLfloat Diffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f };				// Diffuse Light Values
+GLfloat Specular[] = { 1.0, 1.0, 1.0, 1.0 };				// Specular Light Values
+GLfloat Position[] = { 0.0f, 7.0f, -5.0f, 0.0f };			// Light Position
+GLfloat Position2[] = { 0.0f, 0.0f, -5.0f, 1.0f };			// Light Position
+
+GLfloat m_dif1[] = { 0.0f, 0.2f, 1.0f, 1.0f };				// Diffuse Light Values
+GLfloat m_spec1[] = { 0.0, 0.0, 0.0, 1.0 };				// Specular Light Values
+GLfloat m_amb1[] = { 0.0, 0.0, 0.0, 1.0 };				// Ambiental Light Values
+GLfloat m_s1[] = { 18 };
+
+GLfloat m_dif2[] = { 0.8f, 0.2f, 0.0f, 1.0f };				// Diffuse Light Values
+GLfloat m_spec2[] = { 0.0, 0.0, 0.0, 1.0 };				// Specular Light Values
+GLfloat m_amb2[] = { 0.0, 0.0, 0.0, 1.0 };				// Ambiental Light Values
+GLfloat m_s2[] = { 22 };
+
+CTexture text1;
+CTexture text2;
+
+//END NEW//////////////////////////////////////////
+
+CFiguras fig1;
+CFiguras fig2;
+CFiguras fig3;
+CFiguras fig4;	//Pasto01
+CFiguras fig5;	//Casa01
+CFiguras fig6;
+CFiguras fig7;	//Para crear Monito
+
+
+void saveFrame(void)
+{
+
+	printf("frameindex %d\n", FrameIndex);
+
+	KeyFrame[FrameIndex].posX = posX;
+	KeyFrame[FrameIndex].posY = posY;
+	KeyFrame[FrameIndex].posZ = posZ;
+
+	KeyFrame[FrameIndex].rotRodIzq = rotRodIzq;
+	KeyFrame[FrameIndex].giroMonito = giroMonito;
+	KeyFrame[FrameIndex].rotBrIzq = rotBrIzq;
+	KeyFrame[FrameIndex].rotBrDer = rotBrDer;
+	KeyFrame[FrameIndex].rotRodDer = rotRodDer;
+
+	FrameIndex++;
+}
+
+void resetElements(void)
+{
+	posX = KeyFrame[0].posX;
+	posY = KeyFrame[0].posY;
+	posZ = KeyFrame[0].posZ;
+
+	rotRodIzq = KeyFrame[0].rotRodIzq;
+	giroMonito = KeyFrame[0].giroMonito;
+	rotBrIzq = KeyFrame[0].rotBrIzq;
+	rotBrDer = KeyFrame[0].rotBrDer;
+	rotRodDer = KeyFrame[0].rotRodDer;
+
+}
+
+void interpolation(void)
+{
+	KeyFrame[playIndex].incX = (KeyFrame[playIndex + 1].posX - KeyFrame[playIndex].posX) / i_max_steps;
+	KeyFrame[playIndex].incY = (KeyFrame[playIndex + 1].posY - KeyFrame[playIndex].posY) / i_max_steps;
+	KeyFrame[playIndex].incZ = (KeyFrame[playIndex + 1].posZ - KeyFrame[playIndex].posZ) / i_max_steps;
+
+	KeyFrame[playIndex].rotInc = (KeyFrame[playIndex + 1].rotRodIzq - KeyFrame[playIndex].rotRodIzq) / i_max_steps;
+	KeyFrame[playIndex].giroMonitoInc = (KeyFrame[playIndex + 1].giroMonito - KeyFrame[playIndex].giroMonito) / i_max_steps;
+	KeyFrame[playIndex].rotInc2 = (KeyFrame[playIndex + 1].rotBrIzq - KeyFrame[playIndex].rotBrIzq) / i_max_steps;
+	KeyFrame[playIndex].rotInc3 = (KeyFrame[playIndex + 1].rotBrDer - KeyFrame[playIndex].rotBrDer) / i_max_steps;
+	KeyFrame[playIndex].rotInc4 = (KeyFrame[playIndex + 1].rotRodDer - KeyFrame[playIndex].rotRodDer) / i_max_steps;
+
+
+}
+
+
+void monito()
+{
+	//glNewList(1, GL_COMPILE);
+	glPushMatrix();//Pecho
+	glScalef(0.5, 0.5, 0.5);
+	fig7.prisma(2.0, 2.0, 1, text2.GLindex);
+
+	glPushMatrix();//Cuello
+	glTranslatef(0, 1.0, 0.0);
+	fig7.cilindro(0.25, 0.25, 15, 0);
+	glPushMatrix();//Cabeza
+	glTranslatef(0, 1.0, 0);
+	fig7.esfera(0.75, 15, 15, 0);
+	glPopMatrix();
+	glPopMatrix();
+
+	glPushMatrix(); //Brazo derecho-->
+	glTranslatef(1.25, 0.65, 0);
+	fig7.esfera(0.5, 12, 12, 0);
+	glPushMatrix();
+	glTranslatef(0.25, 0, 0);
+	glRotatef(-45, 0, 1, 0);
+	glRotatef(25 + rotBrDer, 0, 0, 1);
+	glTranslatef(0.75, 0, 0);
+	fig7.prisma(0.7, 1.5, 0.7, 0);
+	glPopMatrix();
+	glPopMatrix();
+
+	glPushMatrix(); //Brazo izquierdo <--
+	glTranslatef(-1.25, 0.65, 0);
+	fig7.esfera(0.5, 12, 12, 0);
+	glPushMatrix();
+	glTranslatef(-0.25, 0, 0);
+	glRotatef(45, 0, 1, 0);
+	glRotatef(25 + rotBrIzq, 0, 0, 1);
+	glRotatef(25, 1, 0, 0);
+	glTranslatef(-0.75, 0, 0);
+	fig7.prisma(0.7, 1.5, 0.7, 0);
+	glPopMatrix();
+	glPopMatrix();
+
+	glPushMatrix();//Cintura
+	glColor3f(0, 0, 1);
+	glTranslatef(0, -1.5, 0);
+	fig7.prisma(1, 2, 1, 0);
+
+	glPushMatrix(); //Pie Derecho -->
+	glTranslatef(0.75, -0.5, 0);
+	glRotatef(-15, 1, 0, 0);
+	glTranslatef(0, -0.5, 0);
+	fig7.prisma(1.0, 0.5, 1, 0);
+
+	glPushMatrix();
+	glTranslatef(0, -0.5, 0);
+	glRotatef(15 + rotRodDer, 1, 0, 0);
+	glRotatef(15, 1, 0, 0);
+	glTranslatef(0, -0.75, 0);
+	fig7.prisma(1.5, 0.5, 1, 0);
+
+	glPushMatrix();
+	glTranslatef(0, -0.75, 0.3);
+	fig7.prisma(0.2, 1.2, 1.5, 0);
+	glPopMatrix();
+	glPopMatrix();
+	glPopMatrix();
+
+
+	glPushMatrix(); //Pie Izquierdo -->
+	glTranslatef(-0.75, -0.5, 0);
+	glRotatef(-5, 1, 0, 0);
+	glTranslatef(0, -0.5, 0);
+	fig7.prisma(1.0, 0.5, 1, 0);
+
+	glPushMatrix();
+	glTranslatef(0, -0.5, 0);
+	glRotatef(15 + rotRodIzq, 1, 0, 0);
+	glTranslatef(0, -0.75, 0);
+	fig7.prisma(1.5, 0.5, 1, 0);
+
+	glPushMatrix();
+	glTranslatef(0, -0.75, 0.3);
+	fig7.prisma(0.2, 1.2, 1.5, 0);
+	glPopMatrix();
+	glPopMatrix();
+	glPopMatrix();
+
+
+	glPopMatrix();
+
+
+	glColor3f(1, 1, 1);
+	glPopMatrix();
+	//glEndList();
+}
+
+
+
+
+
+GLuint createDL()
+{
+	GLuint ciudadDL;
+	//GLuint cieloDL;
+
+	// Create the id for the list
+	ciudadDL = glGenLists(1);
+	// start list
+	glNewList(ciudadDL, GL_COMPILE);
+	// call the function that contains 
+	// the rendering commands
+	ciudad();
+	//monito();
+	// endList
+	glEndList();
+
+	return(ciudadDL);
+}
+
+void InitGL(GLvoid)     // Inicializamos parametros
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				// Negro de fondo	
+
+	glEnable(GL_TEXTURE_2D);
+
+	glShadeModel(GL_SMOOTH);
+
+	glLightfv(GL_LIGHT1, GL_POSITION, Position);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, Diffuse);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+
+	glClearDepth(1.0f);									// Configuramos Depth Buffer
+	glEnable(GL_DEPTH_TEST);							// Habilitamos Depth Testing
+	glDepthFunc(GL_LEQUAL);								// Tipo de Depth Testing a realizar
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+	
+
+	//END NEW//////////////////////////////
+
+	objCamera.Position_Camera(0, 2.5f, 3, 0, 2.5f, 0, 0, 1, 0);
+
+	//NEW Crear una lista de dibujo
+	ciudad_display_list = createDL();
+
+	//NEW Iniciar variables de KeyFrames
+	for (int i = 0; i<MAX_FRAMES; i++)
+	{
+		KeyFrame[i].posX = 0;
+		KeyFrame[i].posY = 0;
+		KeyFrame[i].posZ = 0;
+		KeyFrame[i].incX = 0;
+		KeyFrame[i].incY = 0;
+		KeyFrame[i].incZ = 0;
+		KeyFrame[i].rotRodIzq = 0;
+		KeyFrame[i].rotInc = 0;
+		KeyFrame[i].giroMonito = 0;
+		KeyFrame[i].giroMonitoInc = 0;
+		KeyFrame[i].rotInc2 = 0;
+		KeyFrame[i].rotBrIzq = 0;
+	}
+	//NEW//////////////////NEW//////////////////NEW///////////
+
+}
+
+void pintaTexto(float x, float y, float z, void *font, char *string)
+{
+
+	char *c;     //Almacena los caracteres a escribir
+	glRasterPos3f(x, y, z);	//Posicion apartir del centro de la ventana
+							//glWindowPos2i(150,100);
+	for (c = string; *c != '\0'; c++) //Condicion de fin de cadena
+	{
+		glutBitmapCharacter(font, *c); //imprime
+	}
+}
+
+void display(void)   // Creamos la funcion donde se dibuja
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glLoadIdentity();
+	glPushMatrix();
+
+
+	glRotatef(g_lookupdown, 1.0f, 0, 0);
+
+	gluLookAt(objCamera.mPos.x, objCamera.mPos.y, objCamera.mPos.z,
+		objCamera.mView.x, objCamera.mView.y, objCamera.mView.z,
+		objCamera.mUp.x, objCamera.mUp.y, objCamera.mUp.z);
+
+
+	glPopMatrix();
+	glPopMatrix();
+	glDisable(GL_TEXTURE_2D);
+	glDisable(GL_LIGHTING);
+	glColor3f(1.0, 0.0, 0.0);
+	pintaTexto(-11, 12.0, -14.0, (void *)font, "Practica 11");
+	pintaTexto(-11, 8.5, -14, (void *)font, s);
+	glColor3f(1.0, 1.0, 1.0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_TEXTURE_2D);
+
+	glutSwapBuffers();
+
+}
+
+void animacion()
+{
+	fig3.text_izq -= 0.001;
+	fig3.text_der -= 0.001;
+	if (fig3.text_izq<-1)
+		fig3.text_izq = 0;
+	if (fig3.text_der<0)
+		fig3.text_der = 1;
+
+	//Movimiento del monito
+	if (play)
+	{
+
+		if (i_curr_steps >= i_max_steps) //end of animation between frames?
+		{
+			playIndex++;
+			if (playIndex>FrameIndex - 2)	//end of total animation?
+			{
+				printf("termina anim\n");
+				playIndex = 0;
+				play = false;
+			}
+			else //Next frame interpolations
+			{
+				i_curr_steps = 0; //Reset counter
+								  //Interpolation
+				interpolation();
+
+			}
+		}
+		else
+		{
+			//Draw animation
+			posX += KeyFrame[playIndex].incX;
+			posY += KeyFrame[playIndex].incY;
+			posZ += KeyFrame[playIndex].incZ;
+
+			rotRodIzq += KeyFrame[playIndex].rotInc;
+			giroMonito += KeyFrame[playIndex].giroMonitoInc;
+			rotBrIzq += KeyFrame[playIndex].rotInc2;
+			rotBrDer += KeyFrame[playIndex].rotInc3;
+			rotRodDer += KeyFrame[playIndex].rotInc4;
+			i_curr_steps++;
+		}
+
+	}
+
+
+	glutPostRedisplay();
+}
+
+void reshape(int width, int height)   // Creamos funcion Reshape
+{
+	if (height == 0)										// Prevenir division entre cero
+	{
+		height = 1;
+	}
+
+	glViewport(0, 0, width, height);
+
+	glMatrixMode(GL_PROJECTION);						// Seleccionamos Projection Matrix
+	glLoadIdentity();
+
+	// Tipo de Vista
+
+	glFrustum(-0.1, 0.1, -0.1, 0.1, 0.1, 170.0);
+
+	glMatrixMode(GL_MODELVIEW);							// Seleccionamos Modelview Matrix
+	glLoadIdentity();
+}
+
+void keyboard(unsigned char key, int x, int y)  // Create Keyboard Function
+{
+	switch (key) {
+	case 'w':   //Movimientos de camara
+	case 'W':
+		objCamera.Move_Camera(CAMERASPEED + 0.2);
+		break;
+
+	case 's':
+	case 'S':
+		objCamera.Move_Camera(-(CAMERASPEED + 0.2));
+		break;
+
+	case 'a':
+	case 'A':
+		objCamera.Strafe_Camera(-(CAMERASPEED + 0.4));
+		break;
+
+	case 'd':
+	case 'D':
+		objCamera.Strafe_Camera(CAMERASPEED + 0.4);
+		break;
+
+	case 'k': //aqui se guarda el movimiento de animacion
+	case 'K':
+		if (FrameIndex<MAX_FRAMES)
+		{
+			saveFrame();
+		}
+
+		break;
+
+	case 'l': //aqui se da inicio a la rutina de animacion
+	case 'L':
+		if (play == false && (FrameIndex>1))
+		{
+
+			resetElements();
+			//First Interpolation				
+			interpolation();
+
+			play = true;
+			playIndex = 0;
+			i_curr_steps = 0;
+		}
+		else
+		{
+			play = false;
+		}
+		break;
+
+	case 'y': //acercar
+	case 'Y':
+		posZ++;
+		printf("%f \n", posZ);
+		break;
+
+	case 'g': //izquierda
+	case 'G':
+		posX--;
+		printf("%f \n", posX);
+		break;
+
+	case 'h': //alejar
+	case 'H':
+		posZ--;
+		printf("%f \n", posZ);
+		break;
+
+	case 'j': //derecha
+	case 'J':
+		posX++;
+		printf("%f \n", posX);
+		break;
+
+	case 'b': //pie izquierdo hacia atras
+		rotRodIzq++;
+		printf("%f \n", rotRodIzq);
+		break;
+
+	case 'B': //pie izquierdo hacia adelante
+		rotRodIzq--;
+		printf("%f \n", rotRodIzq);
+		break;
+
+	case 'p': //giro completo a la derecha
+		giroMonito++;
+		break;
+
+	case 'P': //giro completo a la izquierda
+		giroMonito--;
+		break;
+
+	case 'i': //brazo izquierdo hacia abajo
+		rotBrIzq++;
+		break;
+
+	case 'I': //brazo izquierdo hacia arriba
+		rotBrIzq--;
+		break;
+
+	case 'o': //brazo derecho hacia arriba
+		rotBrDer++;
+		break;
+
+	case 'O': //brazo derecho hacia abajo
+		rotBrDer--;
+		break;
+
+	case 'm': //pie derecho hacia atras
+		rotRodDer++;
+		break;
+
+	case 'M': //pie derecho hacia adelante
+		rotRodDer--;
+		break;
+
+	case 27:        // Cuando Esc es presionado...
+		exit(0);   // Salimos del programa
+		break;
+	default:        // Cualquier otra
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+void arrow_keys(int a_keys, int x, int y)  // Funcion para manejo de teclas especiales (arrow keys)
+{
+	switch (a_keys) {
+	case GLUT_KEY_PAGE_UP:
+		objCamera.UpDown_Camera(CAMERASPEED);
+		break;
+
+	case GLUT_KEY_PAGE_DOWN:
+		objCamera.UpDown_Camera(-CAMERASPEED);
+		break;
+
+	case GLUT_KEY_UP:     // Presionamos tecla ARRIBA...
+		g_lookupdown -= 1.0f;
+		break;
+
+	case GLUT_KEY_DOWN:               // Presionamos tecla ABAJO...
+		g_lookupdown += 1.0f;
+		break;
+
+	case GLUT_KEY_LEFT:
+		objCamera.Rotate_View(-CAMERASPEED);
+		break;
+
+	case GLUT_KEY_RIGHT:
+		objCamera.Rotate_View(CAMERASPEED);
+		break;
+
+	default:
+		break;
+	}
+	glutPostRedisplay();
+}
+
+void menuKeyFrame(int id)
+{
+	switch (id)
+	{
+	case 0:	//Save KeyFrame
+		if (FrameIndex<MAX_FRAMES)
+		{
+			saveFrame();
+		}
+		break;
+
+	case 1:	//Play animation
+		if (play == false && FrameIndex >1)
+		{
+
+			resetElements();
+			//First Interpolation
+			interpolation();
+
+			play = true;
+			playIndex = 0;
+			i_curr_steps = 0;
+		}
+		else
+		{
+			play = false;
+		}
+		break;
+
+
+	}
+}
+
+
+void menu(int id)
+{
+
+}
+
+
+
+int main(int argc, char** argv)   // Main Function
+{
+	int submenu;
+	glutInit(&argc, argv); // Inicializamos OpenGL
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH); // Display Mode (Clores RGB y alpha | Buffer Doble )
+	glutInitWindowSize(500, 500);	// Tamaño de la Ventana
+	glutInitWindowPosition(0, 0);	//Posicion de la Ventana
+	glutCreateWindow("Practica 11"); // Nombre de la Ventana
+									 //glutFullScreen     ( );         // Full Screen
+	InitGL();						// Parametros iniciales de la aplicacion
+	glutDisplayFunc(display);  //Indicamos a Glut función de dibujo
+	glutReshapeFunc(reshape);	//Indicamos a Glut función en caso de cambio de tamano
+	glutKeyboardFunc(keyboard);	//Indicamos a Glut función de manejo de teclado
+	glutSpecialFunc(arrow_keys);	//Otras
+	glutIdleFunc(animacion);
+
+	submenu = glutCreateMenu(menuKeyFrame);
+	glutAddMenuEntry("Guardar KeyFrame", 0);
+	glutAddMenuEntry("Reproducir Animacion", 1);
+	glutCreateMenu(menu);
+	glutAddSubMenu("Animacion Monito", submenu);
+
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+
+	glutMainLoop();          // 
+
+	return 0;
+}
